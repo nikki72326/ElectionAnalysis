c6bc21d6c9bb447f7ed02f9fb9ec8fe3867720d6
nikki72326 <85453325+nikki72326@users.noreply.github.com>
2021-07-01 21:20:28 -0400

    completed 3.5.5

--- PyPoll.py
+++ PyPoll.py
@@ -3,49 +3,70 @@ import csv
 import os
 
 #Assign a variable for the file to load and the path
-file_to_load = os.path.join("Resources","election_results.csv")
+file_to_load = 'Resources/election_results.csv'
+
 # Create a filename variable to a path to the file to save the analysis data
-file_to_save = os.path.join("analysis","election_analysis.txt")
+file_to_save = 'analysis/election_analysis.txt'
 
 #initialize the total vote counter
 total_votes = 0
 
 #declare a new list
 candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+winning_percentage=0
 
 #Open the election results and read the file.
-with open(file_to_load) as election_data:
-
-#Read the file object with the reader function.
+with open (file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
 #Read the header row.
     headers = next(file_reader)
 
-#Print each row in the CSV file
+#capture the candidate name and total votes for each row
     for row in file_reader:
         total_votes += 1
-          
-#print candidates names
-    candidate_name = row[2]
+        candidate_name = row[2]
 
-if candidate_name not in candidate_options:
-    candidate_options.append(candidate_name)
+    #stop repeat of candidate names  
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
 
-#print outputs
-    print(candidate_options)
+        #start tracking the vote count for each candidate
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
+
+#Get the percentage of votes
+for candidate_name in candidate_votes:
+    votes = candidate_votes [candidate_name]
+    vote_percentage = float(votes) /float(total_votes) * 100
+    
+   #print out each candidates info 
+    print(f"{candidate_name}:{vote_percentage:.1f}% ({votes:,})\n")
+
+    #Determine winning counts
+    if (votes>winning_count) and (vote_percentage> winning_percentage):
+        winning_count = votes
+        winning_percentage = vote_percentage
+        winning_candidate = candidate_name
+
+winning_candidate_summary = (
+    f"------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"  
+    f"------------------------\n") 
+print(winning_candidate_summary)
 
 
-#1. The total number of votes cast
-#2. A complete list of candidates who received votes
-#3. The percentage of votes each candiate won
-#4. The total nmber of votes each candiate won
-#5. The winner of the election based on popular vote.
+
+  #print outputs    
+  
 
 
 
-#open the file to wire data to it
 
-#write in the file
  
 
